56dba9e754ad876810a2de4a151534e91f727a39
GeetaPogul <87964049+GeetaPogul@users.noreply.github.com>
2022-04-30 15:44:34 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Navbar from "./Pages/Nav/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Nav/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <p> Navigation bar </p>;
+};
+
+export default Navbar;
